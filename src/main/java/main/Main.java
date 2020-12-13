b9f9d100c72a2decab522658328b51bd72d94521
MITSIS Nikolaos-Iraklis <nikolaos-iraklis.mitsis@intrasoft-intl.com>
2020-12-13 18:08:08 +0200

    Correct main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
+import models.CellLetter;
 import models.ChessCell;
 
 public class Main {
 
 	public static void main (String [] args) {
+		
+		try {
+			ChessCell startCell = argsToChessCell(args[0]);
+			ChessCell endCell = argsToChessCell(args[1]);
+		} catch (IllegalArgumentException ms) {
+			System.err.println("Wrong arguments" + ms);
+		}
+
 		
 		//improvement, give arguments like start=... end=...
 		// The following line of codes just verify the correction of input arguments 
@@ -16,36 +25,42 @@ public class Main {
 			return;
 		}
 		
-		int startingPointLetter = (int)startingCellStr.toUpperCase().charAt(0);
-		if ( !( startingPointLetter >= 65 || startingPointLetter <= 72 )) {
-			System.out.println("Not a cell point");
-			return;
-		}
+//		ChessCell startCell = argsToChessCell(startingCellStr);
+//		ChessCell endCell = argsToChessCell(endingCellStr);
 		
-		int startingInt = startingCellStr.charAt(1);
-		if ( ! (startingInt < 1 || startingInt > 8 ) ) {
-			System.out.println("Not a valid");
-			return;
-		}
+//		String endXcoordStr = String.valueOf(endingCellStr.toUpperCase().charAt(0));
+//		String endYcoordStr = String.valueOf(endingCellStr.toUpperCase().charAt(1));
 		
-		int endingPointLetter = (int) endingCellStr.toUpperCase().charAt(0);
-		if ( !(endingPointLetter >= 65 || endingPointLetter <= 72)) {
-			System.out.println("Not a cell point");
-			return;
-		}
+//		int startingPointLetter = (int)startingCellStr.toUpperCase().charAt(0);
+//		if ( !( startingPointLetter >= 65 || startingPointLetter <= 72 )) {
+//			System.out.println("Not a cell point");
+//			return;
+//		}
+//		
+//		int startingInt = startingCellStr.charAt(1);
+//		if ( ! (startingInt < 1 || startingInt > 8 ) ) {
+//			System.out.println("Not a valid");
+//			return;
+//		}
+//		
+//		int endingPointLetter = (int) endingCellStr.toUpperCase().charAt(0);
+//		if ( !(endingPointLetter >= 65 || endingPointLetter <= 72)) {
+//			System.out.println("Not a cell point");
+//			return;
+//		}
+//		
+//		int endingInt = endingCellStr.charAt(1);
+//		if ( ! (endingInt < 1 || endingInt > 8 ) ) {
+//			System.out.println("Not a valid");
+//			return;
+//		}
+//		
 		
-		int endingInt = endingCellStr.charAt(1);
-		if ( ! (endingInt < 1 || endingInt > 8 ) ) {
-			System.out.println("Not a valid");
-			return;
-		}
-		
-		
-		ChessCell startingCell = ChessCell.getChessCell(startingPointLetter, startingInt);
-		ChessCell endingCell = ChessCell.getChessCell(endingPointLetter, endingInt);
-		
-		System.out.println("Start point: " + startingCell.toString());
-		System.out.println("Start point: " + endingCell.toString());
+//		ChessCell startingCell = ChessCell.getChessCell(startingPointLetter, startingInt);
+//		ChessCell endingCell = ChessCell.getChessCell(endingPointLetter, endingInt);
+//		
+//		System.out.println("Start point: " + startingCell.toString());
+//		System.out.println("Start point: " + endingCell.toString());
 		
 		
 		/* 
@@ -55,5 +70,21 @@ public class Main {
 		 * https://stackoverflow.com/questions/9689793/cant-execute-jar-file-no-main-manifest-attribute
 		 */
 		
+	}
+
+	private static ChessCell argsToChessCell(String inputArg) {
+		
+		if (inputArg.length() > 2 || inputArg.length() < 1 ) {
+			System.out.println("Wrong input arguments...");
+			throw new IllegalArgumentException("Wrong argument");
+		}
+		
+		String startXcoordStr= String.valueOf(inputArg.toUpperCase().charAt(0));
+		int startXcoord = CellLetter.valueOf(startXcoordStr).ordinal() + 1;
+		
+		String startYcoordStr = String.valueOf(inputArg.toUpperCase().charAt(1));
+		int startYcoord = Integer.parseInt(startYcoordStr);
+		
+		return ChessCell.getChessCell(startXcoord, startYcoord);
 	}
 }
