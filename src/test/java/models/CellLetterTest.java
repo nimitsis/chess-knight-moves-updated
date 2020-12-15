5709775c0bce9500df9639e13273bc264b7a7355
MITSIS Nikolaos-Iraklis <nikolaos-iraklis.mitsis@intrasoft-intl.com>
2020-12-16 01:51:05 +0200

    Test Enum Letters

---
+++ src/test/java/models/CellLetterTest.java
@@ -0,0 +1,33 @@
+package models;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CellLetterTest {
+
+	String a = "A";
+	String f = "F";
+	String w = "W";
+	
+	@Test 
+	public void testLetterA () {
+		CellLetter testA = CellLetter.valueOf("A");
+		assertEquals(a, testA.toString());	
+	}
+	
+	@Test
+	public void testLeterF () {
+		CellLetter testF = CellLetter.valueOf(f);
+		assertEquals(f, testF.toString());
+	}
+	
+	@Test ( expected = IllegalArgumentException.class)
+	public void testLetterW () {
+		CellLetter testW = CellLetter.valueOf(w);
+		assertEquals(w, testW.toString());
+	}
+	
+	
+	
+}
